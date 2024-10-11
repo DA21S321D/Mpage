70d9ee84e871b50ff0e1ccb1486e313349571feb
ceelow <seelowst@gmail.com>
2024-10-12 00:04:05 +0800

    add pics

--- wuwa/js/script.js
+++ wuwa/js/script.js
@@ -27,7 +27,12 @@ function loadRandomBackground() {
         'pic/1.png',
         'pic/2.png',
         'pic/3.png',
-        'pic/4.png'
+        'pic/4.png',
+        'pic/5.png',
+        'pic/6.png',
+        'pic/7.png',
+        'pic/8.png',
+
     ];
 
     const randomIndex = Math.floor(Math.random() * images.length);
